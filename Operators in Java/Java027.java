ca2e1588cca7fb27c8a1a47c79e13ba7bb86e62f
RushiBhosale1 <115489309+RushiBhosale1@users.noreply.github.com>
2023-08-09 21:31:47 +0530

    Add files via upload

---
+++ Operators in Java/Java027.java
@@ -0,0 +1,11 @@
+public class Java027{
+    public static void main(String args[]){
+        int A = 10;
+        int B = 5;
+        System.out.println("add = "+ (A+B));
+        System.out.println("Subtract = "+ (A-B));
+        System.out.println("Multiply = "+ (A*B));
+        System.out.println("Divide = "+ (A/B));
+        System.out.println("Remainder = "+ (A%B));
+    }
+}
