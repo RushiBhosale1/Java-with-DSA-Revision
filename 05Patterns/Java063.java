873e4254b94a6a9095b06a8168f2d719edcd9ea1
RushiBhosale1 <115489309+RushiBhosale1@users.noreply.github.com>
2023-08-12 21:07:08 +0530

    Add files via upload

---
+++ 05Patterns/Java063.java
@@ -0,0 +1,5 @@
+public class Java063 {
+    public static void main(String args[]){
+        
+    }
+}
---
+++ 05Patterns/Java067.java
@@ -0,0 +1,5 @@
+public class Java067 {
+    public static void main(String args[]){
+        
+    }
+}
